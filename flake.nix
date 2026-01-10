329b225d8c12051ab954ea78b1607d2adc30513e
creator54 <hi.creator54@gmail.com>
2026-01-10 11:50:30 +0530

    feat: add nix package definition and wrap scripts

--- flake.nix
+++ flake.nix
@@ -9,8 +9,70 @@
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
+
+      # Define the package
+      yt-bg-pkg = pkgs.stdenv.mkDerivation {
+        pname = "yt-bg";
+        version = "0.1.0";
+
+        src = ./.;
+
+        buildInputs = with pkgs; [ makeWrapper ];
+
+        installPhase = ''
+          mkdir -p $out/bin $out/lib
+
+          # Copy scripts
+          cp bin/yt-bg $out/bin/
+          cp bin/yt-bg-control $out/bin/
+          cp lib/preview.sh $out/lib/
+
+          chmod +x $out/bin/* $out/lib/*
+        '';
+
+        postFixup = ''
+          # Wrap yt-bg with dependencies
+          wrapProgram $out/bin/yt-bg \
+            --prefix PATH : ${pkgs.lib.makeBinPath [
+              pkgs.mpvpaper
+              pkgs.mpv
+              pkgs.yt-dlp
+              pkgs.socat
+              pkgs.jq
+              pkgs.fzf
+              pkgs.curl
+              pkgs.xxd
+              pkgs.coreutils
+              pkgs.libnotify
+              pkgs.procps
+              pkgs.swaybg
+              pkgs.kitty
+              pkgs.gawk
+              pkgs.gnused
+            ]} \
+            --set PREVIEW_SCRIPT_PATH "$out/lib/preview.sh"
+
+          # Wrap yt-bg-control with dependencies
+          wrapProgram $out/bin/yt-bg-control \
+            --prefix PATH : ${pkgs.lib.makeBinPath [
+              pkgs.mpvpaper
+              pkgs.mpv
+              pkgs.yt-dlp
+              pkgs.socat
+              pkgs.jq
+              pkgs.libnotify
+              pkgs.procps
+              pkgs.swaybg
+              pkgs.kitty
+              pkgs.xdg-utils
+            ]} \
+            --set MPRIS_SCRIPT_PATH "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so"
+        '';
+      };
     in
     {
+      packages.${system}.default = yt-bg-pkg;
+
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = with pkgs; [
           # Core
